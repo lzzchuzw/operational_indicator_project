b978e0ad04b1b37ee89ba104adad6c555f848b09
lzzchuzw <297081999@qq.com>
2019-12-01 22:40:54 +0800

    搭建初始项目结构

---
+++ src/main/java/com/shujuhu/operationalindicator/SpringBootOperationalIndicatorApplication.java
@@ -0,0 +1,17 @@
+package com.shujuhu.operationalindicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: SpringBootApplication
+ * @Author: leisure
+ * @Description: 启动程序
+ * @Date: 2019/12/1 22:21
+ */
+@SpringBootApplication
+public class SpringBootOperationalIndicatorApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootOperationalIndicatorApplication.class,args);
+    }
+}
---
+++ src/test/java/com/shujuhu/operationalindicator/SpringBootOperationalIndicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shujuhu.operationalindicator;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @ClassNameame: SpringBootOperationalIndicatorApplicationTests
+ * @Author: leisure
+ * @Description: SpringBoot测试程序
+ * @Date: 2019/12/1 22:26
+ */
+@SpringBootTest
+public class SpringBootOperationalIndicatorApplicationTests {
+}
